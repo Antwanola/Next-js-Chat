1e91dd97218bca9a724a927795491bb969d9821e
Anonymous <fieldagentserve@protonmail.com>
2023-06-09 01:25:50 +0100

    Handling backend messages and recipient

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -2,8 +2,10 @@
 import userResolver from './user.ts';
 //@ts-ignore
 import convoResolver from './conversations.ts'
+//@ts-ignore
+import messageResolver from './messages.ts'
 import merge from "lodash.merge"
 
-const resolvers = merge({}, userResolver, convoResolver)
+const resolvers = merge({}, userResolver, convoResolver, messageResolver)
 
 export default resolvers
